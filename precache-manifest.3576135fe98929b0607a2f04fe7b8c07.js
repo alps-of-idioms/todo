2e990d41b762f3fc4e0702e7d908665dcd7c9511
Ivan <cellarrainbow@gmail.com>
2019-02-28 18:37:30 +0300

    Updates

---
+++ precache-manifest.3576135fe98929b0607a2f04fe7b8c07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b16784f5aabc2900b92",
+    "url": "/todo/static/js/runtime~main.3b16784f.js"
+  },
+  {
+    "revision": "cb234978ddefc930c610",
+    "url": "/todo/static/js/main.cb234978.chunk.js"
+  },
+  {
+    "revision": "67f4206207c806f30377",
+    "url": "/todo/static/js/2.67f42062.chunk.js"
+  },
+  {
+    "revision": "cb234978ddefc930c610",
+    "url": "/todo/static/css/main.5037ac30.chunk.css"
+  },
+  {
+    "revision": "3e346d0f05e9237c035bb3efb8c07e8a",
+    "url": "/todo/index.html"
+  }
+];
--- src/components/search-pannel/index.js
+++
@@ -1,3 +0,0 @@
-import SearchPanel from "./search-pannel";
-
-export default SearchPanel;
